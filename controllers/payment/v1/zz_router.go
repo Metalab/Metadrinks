85bcd5b17d35376de0a58a520049aa0613286971
Melina <142022253+mel1na@users.noreply.github.com>
2025-07-07 22:37:09 +0200

    implement auth for sensitive reader endpoints

--- controllers/payment/v1/zz_router.go
+++ controllers/payment/v1/zz_router.go
@@ -1,6 +1,9 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"metalab/drinks-pos/controllers/auth"
+)
 
 func RegisterRoutesV1(r *gin.RouterGroup) {
 	r.POST("/callback", GetIncomingWebhook)
@@ -9,8 +12,8 @@ func RegisterRoutesV1(r *gin.RouterGroup) {
 	re := r.Group("readers")
 	re.GET("/", FindReaders)
 	re.GET("/:id", FindReader)
-	re.GET("/api", FindApiReaders)
-	re.POST("/link", CreateReader)
+	re.GET("/api", auth.JWTAuthMiddleware.MiddlewareFunc(), FindApiReaders)
+	re.POST("/link", auth.JWTAuthMiddleware.MiddlewareFunc(), CreateReader)
 	re.DELETE("/terminate", TerminateReaderCheckout)
-	re.DELETE("/unlink", UnlinkReader)
+	re.DELETE("/unlink", auth.JWTAuthMiddleware.MiddlewareFunc(), UnlinkReader)
 }
