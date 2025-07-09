b19c6afd90aa81231a52039d418326b75026d3a7
Melina <142022253+mel1na@users.noreply.github.com>
2025-07-09 19:00:37 +0200

    enforce admin on more endpoints

--- controllers/api/v1/zz_router.go
+++ controllers/api/v1/zz_router.go
@@ -1,21 +1,24 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"metalab/drinks-pos/controllers/auth"
+)
 
 func RegisterRoutesV1(r *gin.RouterGroup) {
 	i := r.Group("items")
 	i.GET("/", FindItems)
 	i.GET("/:id", FindItem)
-	i.POST("/", CreateItem)
-	i.PUT("/:id", UpdateItem)
-	i.DELETE("/:id", DeleteItem)
+	i.POST("/", auth.JWTAuthMiddleware.MiddlewareFunc(), CreateItem)
+	i.PUT("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), UpdateItem)
+	i.DELETE("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), DeleteItem)
 
 	u := r.Group("users")
 	u.POST("/", CreateUser)
 	u.GET("/", FindUsers)
 	u.GET("/:id", FindUser)
-	u.PUT("/:id", UpdateUser)
-	u.DELETE("//:id", DeleteUser)
+	u.PUT("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), UpdateUser)
+	u.DELETE("//:id", auth.JWTAuthMiddleware.MiddlewareFunc(), DeleteUser)
 
 	p := r.Group("purchases")
 	p.POST("/", CreatePurchase)
