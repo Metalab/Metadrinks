9a2607ca7152121d8294f00bed8a444a43bc5aa1
Melina <142022253+mel1na@users.noreply.github.com>
2025-07-09 18:17:27 +0200

    add new attributes to user object

--- models/user.go
+++ models/user.go
@@ -10,10 +10,12 @@ import (
 type User struct {
 	UserID    uuid.UUID      `json:"id" gorm:"primaryKey;unique;type:uuid;default:gen_random_uuid()"`
 	Name      string         `json:"name" gorm:"index,unique"`
+	Image     string         `json:"image" default:"assets/empty.webp"`
 	Password  string         `json:"password,omitempty"`
 	Balance   int            `json:"balance" gorm:"default:0"`
 	IsTrusted bool           `json:"is_trusted" gorm:"default:false"`
 	IsAdmin   bool           `json:"is_admin" gorm:"default:false"`
+	IsActive  bool           `json:"is_active" gorm:"default:true"`
 	CreatedAt time.Time      `json:"created_at"`
 	UsedAt    time.Time      `json:"used_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at"`
