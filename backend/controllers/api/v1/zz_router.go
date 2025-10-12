4f5707e2e7bcec26c92aa868c1e5a11499ddcef7
Melina <142022253+mel1na@users.noreply.github.com>
2025-10-12 23:37:24 +0200

    change paths so they make more sense

--- backend/controllers/api/v1/zz_router.go
+++ backend/controllers/api/v1/zz_router.go
@@ -8,22 +8,22 @@ import (
 
 func RegisterRoutesV1(r *gin.RouterGroup) {
 	i := r.Group("items")
-	i.GET("/", FindItems)
+	i.GET("", FindItems)
 	i.GET("/:id", FindItem)
-	i.POST("/", auth.JWTAuthMiddleware.MiddlewareFunc(), auth.IsUserAdmin(), CreateItem)
+	i.POST("", auth.JWTAuthMiddleware.MiddlewareFunc(), auth.IsUserAdmin(), CreateItem)
 	i.PUT("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), auth.IsUserAdmin(), UpdateItem)
 	i.DELETE("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), auth.IsUserAdmin(), DeleteItem)
 
 	u := r.Group("users")
-	u.POST("/", CreateUser)
-	u.GET("/", FindUsers)
+	u.POST("", CreateUser)
+	u.GET("", FindUsers)
 	u.GET("/:id", FindUser)
 	//u.PUT("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), auth.IsUserAdmin(), UpdateUser)
 	//u.DELETE("//:id", auth.JWTAuthMiddleware.MiddlewareFunc(), auth.IsUserAdmin(), DeleteUser)
 
 	p := r.Group("purchases")
-	p.POST("/", auth.JWTAuthMiddleware.MiddlewareFunc(), CreatePurchase)
-	p.GET("/", auth.JWTAuthMiddleware.MiddlewareFunc(), FindPurchases)
+	p.POST("", auth.JWTAuthMiddleware.MiddlewareFunc(), CreatePurchase)
+	p.GET("", auth.JWTAuthMiddleware.MiddlewareFunc(), FindPurchases)
 	p.GET("/:id", auth.JWTAuthMiddleware.MiddlewareFunc(), FindPurchase)
 	//p.PATCH("/:id", UpdatePurchase)
 	//p.DELETE("/:id", DeletePurchase)
