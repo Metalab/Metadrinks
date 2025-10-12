690b3fdefe0b48e489d64f1c75a4c3eb8d2b8816
Melina <142022253+mel1na@users.noreply.github.com>
2025-10-12 07:03:51 +0200

    properly set default for item image

--- backend/models/item.go
+++ backend/models/item.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type Item struct {
 	ItemId  uuid.UUID `json:"id" gorm:"primaryKey;unique;type:uuid;default:gen_random_uuid()" example:"00000000-0000-0000-0000-000000000000"`
 	Name    string    `json:"name" gorm:"unique"`
-	Image   string    `json:"image" default:"assets/empty.webp"`
+	Image   string    `json:"image" gorm:"default:assets/empty.webp"`
 	Price   uint      `json:"price"`
 	Barcode string    `json:"barcode,omitempty" gorm:"size:13"` //barcodes are 13 numbers long
 }
