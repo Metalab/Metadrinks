687c734a2c52b3431312b6029da136decc444f3f
Melina <142022253+mel1na@users.noreply.github.com>
2025-10-15 13:28:05 +0200

    drop sse clients when unreachable/disconnected

--- backend/controllers/payment/v1/sse.go
+++ backend/controllers/payment/v1/sse.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"io"
 	"log"
+	"time"
 
 	sumupmodels "metalab/metadrinks/models/sumup"
 
@@ -10,10 +11,11 @@ import (
 )
 
 type Event struct {
-	Message       chan string
-	NewClients    chan chan string
-	ClosedClients chan chan string
-	TotalClients  map[chan string]bool
+	Message           chan string
+	NewClients        chan chan string
+	ClosedClients     chan chan string
+	TotalClients      map[chan string]bool
+	HeartbeatInterval time.Duration
 }
 
 type ClientChan chan string
@@ -33,6 +35,9 @@ func NewServer() *Event {
 }
 
 func (Stream *Event) listen() {
+	heartbeat := time.NewTicker(30 * time.Second)
+	defer heartbeat.Stop()
+
 	for {
 		select {
 		case client := <-Stream.NewClients:
@@ -40,16 +45,47 @@ func (Stream *Event) listen() {
 			log.Printf("Client added. %d registered clients", len(Stream.TotalClients))
 
 		case client := <-Stream.ClosedClients:
-			delete(Stream.TotalClients, client)
-			close(client)
-			log.Printf("Removed client. %d registered clients", len(Stream.TotalClients))
+			if _, ok := Stream.TotalClients[client]; ok {
+				delete(Stream.TotalClients, client)
+				close(client)
+				log.Printf("Removed client. %d registered clients", len(Stream.TotalClients))
+			}
 
 		case eventMsg := <-Stream.Message:
 			for clientMessageChan := range Stream.TotalClients {
 				select {
 				case clientMessageChan <- eventMsg:
 				default:
+					log.Printf("Client channel blocked, removing")
+					delete(Stream.TotalClients, clientMessageChan)
+					close(clientMessageChan)
 				}
+			}
+
+		case <-heartbeat.C:
+			if len(Stream.TotalClients) == 0 {
+				continue
+			}
+
+			log.Printf("Sending heartbeat to %d clients", len(Stream.TotalClients))
+			var disconnectedClients []chan string
+
+			for clientMessageChan := range Stream.TotalClients {
+				select {
+				case clientMessageChan <- `{"type":"heartbeat","data":{"timestamp":"` + time.Now().Format(time.RFC3339) + `"}}`:
+				default:
+					log.Printf("Client not responding to heartbeat, marking for removal")
+					disconnectedClients = append(disconnectedClients, clientMessageChan)
+				}
+			}
+
+			for _, client := range disconnectedClients {
+				delete(Stream.TotalClients, client)
+				close(client)
+			}
+
+			if len(disconnectedClients) > 0 {
+				log.Printf("Removed %d disconnected clients. %d clients remaining", len(disconnectedClients), len(Stream.TotalClients))
 			}
 		}
 	}
@@ -99,22 +135,44 @@ func SSEHeadersMiddleware() gin.HandlerFunc {
 
 func (Stream *Event) ServeHTTP() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		clientChan := make(ClientChan)
+		// buffered channel to prevent blocking
+		clientChan := make(ClientChan, 10)
 		Stream.NewClients <- clientChan
 
+		c.SSEvent("onopen", gin.H{
+			"type": "connection_established",
+			"data": gin.H{
+				"status":    "connected",
+				"timestamp": time.Now().Unix(),
+			},
+		})
+		c.Writer.Flush()
+
+		defer func() {
+			Stream.ClosedClients <- clientChan
+		}()
+
+		// Monitor for client disconnect
+		notify := c.Writer.CloseNotify()
 		go func() {
-			<-c.Writer.CloseNotify()
-			for range clientChan {
-			}
+			<-notify
+			log.Printf("Client disconnected (CloseNotify)")
 			Stream.ClosedClients <- clientChan
 		}()
 
 		c.Stream(func(w io.Writer) bool {
-			if msg, ok := <-clientChan; ok {
+			select {
+			case msg, ok := <-clientChan:
+				if !ok {
+					log.Printf("Client channel closed")
+					return false
+				}
 				c.SSEvent("message", msg)
 				return true
+			case <-notify:
+				log.Printf("Client disconnected during stream")
+				return false
 			}
-			return false
 		})
 	}
 }
